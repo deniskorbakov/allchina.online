e2a773b06a89b0f5c9c7f7835c59a835c9328b98
deniskorbakov <korbakovd@gmail.com>
2023-01-15 00:03:55 +0300

    сократил скорость загрузки

--- slider.php
+++ slider.php
@@ -5,31 +5,43 @@
       <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
       <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="img\slider1.jpg" class="img-fluid rounded mx-auto d-block " width="1280px" height="720px" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Участок Великой Китайской стены Мутяньюй</h5>
-          <p>Тем, кто решил поехать в это чудесное государство Востока, обязательно нужно посетить Великую Китайскую стену</p>
+
+    <div class="container-fluid">
+        <div class="carousel-inner">
+        <div class="carousel-item active">
+            <div class="container">
+            <img src="img/slider1.WAxce.jpg" loading="lazy" class="img-thumbnail" width="1280px" height="720px" alt="...">
+            </div>
+            <div class="carousel-caption d-none d-md-block">
+            <h5>Участок Великой Китайской стены Мутяньюй</h5>
+            <p>Тем, кто решил поехать в это чудесное государство Востока, обязательно нужно посетить Великую Китайскую стену</p>
+            </div>
+        </div> 
+
+        <div class="carousel-item">
+            <div class="container">
+            <img src="img/slider2.3wli3.jpg" loading="lazy" class="img-thumbnail" width="1280px" height="720px" alt="...">
+            </div>
+            <div class="carousel-caption d-none d-md-block">
+            <h5>Большой Будда</h5>
+            <p>Тяньтань Будда, известен также как Большой Будда (кит. трад. 天壇大佛, упр. 天坛大佛) — 
+                большая бронзовая статуя Будды, расположена в Гонконге, на острове Лантау, вблизи монастыря По Лин</p>
+            </div>
+            
         </div>
-      </div>
-      <div class="carousel-item">
-        <img src="img\slider2.jpg" class="img-fluid rounded mx-auto d-block" width="1280px" height="720px" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Большой Будда</h5>
-          <p>Тяньтань Будда, известен также как Большой Будда (кит. трад. 天壇大佛, упр. 天坛大佛) — 
-            большая бронзовая статуя Будды, расположена в Гонконге, на острове Лантау, вблизи монастыря По Лин</p>
+        <div class="carousel-item">
+            <div class="container">
+            <img src="img/slider3.mXGqT.jpg" loading="lazy" class="img-thumbnail" width="1280px" height="720px" alt="...">
+            </div>
+            <div class="carousel-caption d-none d-md-block">
+            <h5>Террасы Лонгджи </h5>
+            <p>Рисовые поля Лунцзи охраняются государством и признаны национальным парком. Небольшие 
+                каменные тропинки соединяют все холмы, так что вы можете добраться по ним в любую точку</p>
+            </div>
         </div>
-      </div>
-      <div class="carousel-item">
-        <img src="img\slider3.jpg" class="img-fluid rounded mx-auto d-block" width="1280px" height="720px" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Террасы Лонгджи </h5>
-          <p>Рисовые поля Лунцзи охраняются государством и признаны национальным парком. Небольшие 
-            каменные тропинки соединяют все холмы, так что вы можете добраться по ним в любую точку</p>
         </div>
-      </div>
     </div>
+
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Предыдущий</span>
@@ -40,3 +52,4 @@
     </button>
   </div>
 </div>
+
