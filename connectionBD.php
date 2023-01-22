506cb5fb53c5c17432302c72991b6524610fb4f3
deniskorbakov <korbakovd@gmail.com>
2023-01-22 17:20:24 +0300

    тизменил пароль

--- connectionBD.php
+++ connectionBD.php
@@ -4,5 +4,5 @@
 
 
 // Подключение к бд по хостингу
-$mysql = new mysqli('localhost', 'u1878513_default', 'nJ7uB2kK6ayM0hO0', 'u1878513_default');
+$mysql = new mysqli('localhost', 'u1878513_default', '5aMSXwf0s69Y4CB5', 'u1878513_default');
 ?>
